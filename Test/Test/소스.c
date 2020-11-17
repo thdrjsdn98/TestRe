5879490119e85d1d6c8e9b6b34172a255e04c226
송건우 <xyeae@naver.com>
2020-11-18 00:57:09 +0900

    과제 내용 풀어보기

--- Test/Test/소스.c
+++ Test/Test/소스.c
@@ -2,31 +2,91 @@
 
 int main()
 {
+	// 두개의 정수를 입력받아서
+		// 사칙연산을 하는 함수를 하나씩 만들어주세요
+		// plus 함수
+		// minus 함수
+
 	/*int a, b;
+	printf("두개의 정수를 입력하세요: ");
 	scanf_s("%d %d", &a, &b);
-	printf("%d", a + b);*/
 
-	int arr[5];
-	int max, min, sum, i;
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d", a, b, a / b);*/
 
-	for (i = 0; i < 5; i++)
-	{
-		printf("입력: ");
-		scanf_s("%d", &arr[i]);
-	}
+	// 1. 랜덤으로 받은 배열의 값중에 함수로
+	// 가장 큰 값 위치 리턴하기
 
-	max = min = sum = arr[0];
-	for (i = 1; i < 5; i++)
-	{
-		sum += arr[i];
-		if (max < arr[i])
-			max = arr[i];
-		if (min > arr[i])
-			min = arr[i];
-	}
 
-	printf("최대값: %d \n", max);
-	printf("최소값: %d \n", min);
-	printf("총 합: %d \n", sum);
+
+	// 2. 랜덤으로 받은 배열의 값중에 함수로
+	// 가장 작은 값 리턴하기
+
+
+
+	// 3. 함수로 hello 또는 world 출력하기
+	// 1 이 입력되면 hello, 2 가 입력되면 world 출력
+
+	/*char ch;
+	printf("1 또는 2를 입력하세요: ");
+	scanf_s("%c", &ch);
+
+	if (ch == '1')
+		printf("hello\n");
+	if (ch == '2')
+		printf("world\n");*/
+
+	// 4. 함수로 짝수 또는 홀수 출력하기
+	// 홀수가 입력되면 odd, 짝수가 입력되면 even 출력
+
+	/*int num;
+	printf("정수를 입력하세요: ");
+	scanf_s("%d", &num);
+
+	if (num % 2 != 0)
+		printf("odd\n");
+	else
+		printf("even\n");*/
+
+	// 5. 함수로 false 또는 true 출력하기
+	// 0 이 입력되면 false, 그 외에는 true 출력
+
+	/*int a;
+	printf("숫자를 입력하세요: ");
+	scanf_s("%d", &a);
+
+	if (a != 0)
+		printf("true\n");
+	else
+		printf("false\n");*/
+
+
+	// 두 정수(a, b)를 입력받아
+	// a가 b보다 크면 1을, a가 b보다 작거나 같으면 0을 출력
+
+	/*int a, b;
+	printf("두 정수를 입력하세요: ");
+	scanf_s("%d %d", &a, &b);
+
+	if (a > b)
+		printf("1\n");
+	else
+		printf("0\n");*/
+
+	// 두 정수(a, b)를 입력받아
+	// b가 a보다 크거나 같으면 1을,
+	// 그렇지 않으면 0을 출력
+
+	/*int a, b;
+	printf("두 정수를 입력하세요: ");
+	scanf_s("%d %d", &a, &b);
+
+	if (b >= a)
+		printf("1\n");
+	else
+		printf("0\n");*/
+
 	return 0;
 }
