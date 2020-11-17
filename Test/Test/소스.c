528ac11664be045b885c7ff42c5a097bc634ae99
송건우 <xyeae@naver.com>
2020-11-18 00:18:47 +0900

    배열로 예제 풀어보기

--- Test/Test/소스.c
+++ Test/Test/소스.c
@@ -2,7 +2,31 @@
 
 int main()
 {
-	int a, b;
+	/*int a, b;
 	scanf_s("%d %d", &a, &b);
-	printf("%d", a + b);
+	printf("%d", a + b);*/
+
+	int arr[5];
+	int max, min, sum, i;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("입력: ");
+		scanf_s("%d", &arr[i]);
+	}
+
+	max = min = sum = arr[0];
+	for (i = 1; i < 5; i++)
+	{
+		sum += arr[i];
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+	}
+
+	printf("최대값: %d \n", max);
+	printf("최소값: %d \n", min);
+	printf("총 합: %d \n", sum);
+	return 0;
 }
